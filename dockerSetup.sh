f22d621c4b268d7ef8517e6916550dec2cfad6de
daisyLsbu <mymail.daisy@gmail.com>
2024-01-13 14:49:31 +0000

    dockercmds

--- dockerSetup.sh
+++ dockerSetup.sh
@@ -1,11 +1,26 @@
 # this file can help setup docker and start the containers after downloading the prebuild container with existing stress python file to randomly stress the container.
 
 #install docker
-#add user to group
+sudo apt -y install docker.io
+#adding user to the groups 
+sudo usermod -aG ubridge user
+sudo usermod -aG libvirt user 
+sudo usermod -aG kvm user
+sudo usermod -aG wireshark user
+sudo usermod -aG docker user
+docker run hello-world #test docker
+docker run -it ubuntu:latest  #to pull the image from internet 
+docker stop <>
+docker rm <>
 #docker login
+#create custom image
+docker build -t my_image .  # this will create an image with name "my_image" and it's tag is latest, if you want to give a different
+#checkin image to github 
+docker commit <container id> username/repository:tag
 #pull image from docker hub
+docker pull username/repository:tag
 #run container with volume mounting
-sudo docker run -d 86a171db06e1 python3 looprandomstress.py
+sudo docker run -d 86a171db06e1 python3 looprandomstress.py -- starting the  same image again using image id
 #copy migration script outside the folder
 cp migrateVictim.py '/home/ubuntu/.'
 cp transferimage.py '/home/ubuntu/.'
